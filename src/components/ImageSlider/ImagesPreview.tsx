e88da050bec07a7d317ccdf436a543b834db865c
kingwell47 <joeldoctor47@gmail.com>
2021-10-25 19:43:10 +0800

    added first image and slider buttons

--- src/components/ImageSlider/ImagesPreview.tsx
+++ src/components/ImageSlider/ImagesPreview.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function ImagesPreview() {
   return (
-    <div className='slider__previews'>
+    <div className='slider__previews hide-for-mobile'>
       <img src='' alt='preview1' className='slider__preview' />
       <img src='' alt='preview2' className='slider__preview' />
       <img src='' alt='preview3' className='slider__preview' />
--- src/components/ImageSlider/index.tsx
+++ src/components/ImageSlider/index.tsx
@@ -1,10 +1,39 @@
 import React from "react";
 import ImagesPreview from "./ImagesPreview";
+import "./ImageSlider.scss";
 
 function ImageSlider() {
   return (
     <div className='slider'>
-      <img src='' alt='product' className='slider__display' />
+      <div className='slider__display'>
+        <img
+          src={require("../../images/image-product-1.jpg").default}
+          alt='product'
+          className='slider__image'
+        />
+      </div>
+      <button className='slider__left hide-for-desktop'>
+        <svg width='13' height='18' xmlns='http://www.w3.org/2000/svg'>
+          <path
+            d='m2 1 8 8-8 8'
+            stroke='#1D2026'
+            stroke-width='3'
+            fill='none'
+            fill-rule='evenodd'
+          />
+        </svg>
+      </button>
+      <button className='slider__right hide-for-desktop'>
+        <svg width='13' height='18' xmlns='http://www.w3.org/2000/svg'>
+          <path
+            d='m2 1 8 8-8 8'
+            stroke='#1D2026'
+            stroke-width='3'
+            fill='none'
+            fill-rule='evenodd'
+          />
+        </svg>
+      </button>
       <ImagesPreview />
     </div>
   );
