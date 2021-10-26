80ed158a0736fcb7b108f4aaf0cdcd07fb9bc121
kingwell47 <joeldoctor47@gmail.com>
2021-10-26 16:15:18 +0800

    implemented mobile image slider

--- src/components/ImageSlider/index.tsx
+++ src/components/ImageSlider/index.tsx
@@ -1,38 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import ImagesPreview from "./ImagesPreview";
 import "./ImageSlider.scss";
 
-function ImageSlider() {
+const IMAGES: string[] = [
+  "../../images/image-product-1.jpg",
+  "../../images/image-product-2.jpg",
+  "../../images/image-product-3.jpg",
+  "../../images/image-product-4.jpg",
+];
+
+function ImageSlider(): JSX.Element {
+  const [current, setCurrent] = useState<number>(0);
+
+  const onNext = (): void => {
+    setCurrent(current === IMAGES.length - 1 ? 0 : current + 1);
+  };
+
+  const onPrevious = (): void => {
+    setCurrent(current === 0 ? IMAGES.length - 1 : current - 1);
+  };
+
   return (
     <div className='slider'>
       <div className='slider__display'>
-        <img
-          src={require("../../images/image-product-1.jpg").default}
-          alt='product'
-          className='slider__image'
-        />
+        <div className='slider__slides'>
+          {IMAGES.map(
+            (image: string, index: number): JSX.Element => (
+              <div
+                className={
+                  index === current ? "slider__slide active" : "slider__slide"
+                }
+                key={index}>
+                {index === current && (
+                  <img
+                    src={
+                      require(`../../images/image-product-${index + 1}.jpg`)
+                        .default
+                    }
+                    alt='product'
+                    className='slider__image'
+                  />
+                )}
+              </div>
+            )
+          )}
+        </div>
       </div>
-      <button className='slider__left hide-for-desktop'>
-        <svg width='13' height='18' xmlns='http://www.w3.org/2000/svg'>
-          <path
-            d='m2 1 8 8-8 8'
-            stroke='#1D2026'
-            stroke-width='3'
-            fill='none'
-            fill-rule='evenodd'
-          />
-        </svg>
+      <button className='slider__left hide-for-desktop' onClick={onPrevious}>
+        <img
+          src={require("../../images/icon-previous.svg").default}
+          alt='next'
+        />
       </button>
-      <button className='slider__right hide-for-desktop'>
-        <svg width='13' height='18' xmlns='http://www.w3.org/2000/svg'>
-          <path
-            d='m2 1 8 8-8 8'
-            stroke='#1D2026'
-            stroke-width='3'
-            fill='none'
-            fill-rule='evenodd'
-          />
-        </svg>
+      <button className='slider__right hide-for-desktop' onClick={onNext}>
+        <img
+          src={require("../../images/icon-next.svg").default}
+          alt='previous'
+        />
       </button>
       <ImagesPreview />
     </div>
