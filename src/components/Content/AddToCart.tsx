45eaa38e0f086fe2f7b2da2273a94e4aa74a15bb
kingwell47 <joeldoctor47@gmail.com>
2021-10-28 11:59:18 +0800

    done with mobile styling

--- src/components/Content/AddToCart.tsx
+++ src/components/Content/AddToCart.tsx
@@ -4,11 +4,28 @@ function AddToCart() {
   return (
     <div className='content__add_to_cart'>
       <div className='content__amount_wrapper'>
-        <button className='content__amount_button'>-</button>
+        <button className='content__amount_button'>
+          <img
+            src={require("../../images/icon-minus.svg").default}
+            alt='reduce amount'
+          />
+        </button>
         <div className='content__amount'>0</div>
-        <button className='content__amount_button'>+</button>
+        <button className='content__amount_button'>
+          <img
+            src={require("../../images/icon-plus.svg").default}
+            alt='add amount'
+          />
+        </button>
       </div>
-      <button className='content__cart_button'>Add to cart</button>
+      <button className='content__cart_button'>
+        <img
+          src={require("../../images/icon-cart-light.svg").default}
+          alt=''
+          className='content__cart_button_icon'
+        />
+        Add to cart
+      </button>
     </div>
   );
 }
